90437a4aa7ef11e2dd3048068843f83b915c3f3d
Carlos Triana <carlostriana404@gmail.com>
2024-05-20 21:11:29 -0500

    las rutas devuelven ahora formato json

--- src/index.js
+++ src/index.js
@@ -3,20 +3,19 @@ const app = express();
 
     //ruta, callback
 app.get( '/', function( request, response ){
-    const homepage = `<h1>home page</h1>
-    <p>esta es la pagina de inicio de la tienda virtual</p>`
-    response.send( homepage);
+    const data = {
+        message: 'Bienvenido a mi web'
+    }
+
+    response.json(data);
 });
-app.get( '/contactenos', function(request, response){
-    response.send(`<h1>contact page</h1>
-    <form>
-        <label>
-            Nombre
-        </label>
-        <input />
-        <button>send</button>
-    </form>`
-);
+app.get( '/api/user', function(request, response){
+    response.json([
+        {nombre:'Maria Alejandra', genero: 'Femenino'},
+        {nombre:'Nidia', genero: 'Femenino'},
+        {nombre:'Laura', genero: 'Femenino'},
+        {nombre:'Natalia', genero: 'Femenino'}
+    ]);
 } );
            //purto, callback
 app.listen(3000, function(){
