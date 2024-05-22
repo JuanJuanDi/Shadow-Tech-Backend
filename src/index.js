ccc029182298b717a7eb21d0db562427f749f785
Carlos Triana <carlostriana404@gmail.com>
2024-05-21 20:44:21 -0500

    [/] separamos las rutas que definen el crud para la entidad producto

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 const express = require( 'express' );
 const app = express();
+        //ruta, importacion
+        //->http://locahost:3000/apis/product/
+app.use('/api/products' , require('./routes/product.routes'))
 
-    //ruta, callback
-app.get( '/', function( request, response ){
-    const data = {
-        message: 'Bienvenido a mi web'
-    }
 
-    response.json(data);
-});
-app.get( '/api/user', function(request, response){
-    response.json([
-        {nombre:'Maria Alejandra', genero: 'Femenino'},
-        {nombre:'Nidia', genero: 'Femenino'},
-        {nombre:'Laura', genero: 'Femenino'},
-        {nombre:'Natalia', genero: 'Femenino'}
-    ]);
-} );
            //purto, callback
 app.listen(3000, function(){
     console.log('Servidor corriendo en el puerto 3000')
